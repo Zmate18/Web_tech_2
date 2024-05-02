46420fa3f5a0bd5a88043cc7f27d0a3234ae9223
Zmate18 <mate.zavarko18@gmail.com>
2024-05-02 18:47:15 +0200

    Update cart page component to display cart items and allow modification

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -8,7 +8,7 @@ import { CartItem } from '../shared/models/CartItem';
   providedIn: 'root'
 })
 export class CartService {
-  private cart:Cart = this.getCartFromLocalStorage();
+  private cart: Cart = new Cart();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
   constructor() { }
 
@@ -19,13 +19,13 @@ export class CartService {
       return;
 
     this.cart.items.push(new CartItem(food));
-    this.setCartToLocalStorage();
+    this.updateCart();
   }
 
   removeFromCart(foodId: string): void {
     this.cart.items = this.cart.items
       .filter(item => item.food.id != foodId);
-    this.setCartToLocalStorage();
+      this.updateCart();
   }
 
   changeQuantity(foodId: string, quantity: number) {
@@ -35,31 +35,25 @@ export class CartService {
 
     cartItem.quantity = quantity;
     cartItem.price = quantity * cartItem.food.price;
-    this.setCartToLocalStorage();
+    this.updateCart();
   }
 
   clearCart() {
     this.cart = new Cart();
-    this.setCartToLocalStorage();
+    this.updateCart();
   }
 
   getCartObservable(): Observable<Cart> {
     return this.cartSubject.asObservable();
   }
 
-  private setCartToLocalStorage(): void {
-    this.cart.totalPrice = this.cart.items
-      .reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
-    this.cart.totalCount = this.cart.items
-      .reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0);
-
-    const cartJson = JSON.stringify(this.cart);
-    localStorage.setItem('Cart', cartJson);
-    this.cartSubject.next(this.cart);
+  getCart(): Cart{
+    return this.cartSubject.value;
   }
 
-  private getCartFromLocalStorage(): Cart {
-    const cartJson = localStorage.getItem('Cart');
-    return cartJson ? JSON.parse(cartJson) : new Cart();
+  private updateCart(): void {
+    this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
+    this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0);
+    this.cartSubject.next(this.cart);
   }
 }
