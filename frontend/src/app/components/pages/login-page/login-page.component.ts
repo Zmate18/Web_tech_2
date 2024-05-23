1782cc820fcf44794b79315ccfeb55d73d56107a
Zmate18 <mate.zavarko18@gmail.com>
2024-05-23 19:13:43 +0200

    Add input validation

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
   loginForm!: FormGroup;
