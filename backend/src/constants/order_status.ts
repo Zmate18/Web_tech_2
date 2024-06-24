eb07b132572e222a7c8aa2ffb61a5f9cf8b4beba
Zmate18 <mate.zavarko18@gmail.com>
2024-06-24 19:23:25 +0200

    Create order

---
+++ backend/src/constants/order_status.ts
@@ -0,0 +1,7 @@
+export enum OrderStatus {
+  NEW = "Új rendelés",
+  PAYED = "Kifizetve",
+  SHIPPED = "Kiszállítva",
+  CANCELED = "Törölve",
+  REFUNDED = "Visszafizetve",
+}
--- frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -1,33 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Order } from '../../../shared/models/Order';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartService } from '../../../services/cart.service';
 import { UserService } from '../../../services/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { OrderService } from '../../../services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
-  styleUrl: './checkout-page.component.css',
+  styleUrls: ['./checkout-page.component.css']
 })
-export class CheckoutPageComponent {
-  order: Order = new Order();
+export class CheckoutPageComponent implements OnInit {
+  order:Order = new Order();
   checkoutForm!: FormGroup;
-  constructor(
-    cartService: CartService,
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private toastrService: ToastrService
-  ) {
-    const cart = cartService.getCart();
-    this.order.items = cart.items;
-    this.order.totalPrice = cart.totalPrice;
-  }
+  constructor(cartService:CartService,
+              private formBuilder: FormBuilder,
+              private userService: UserService,
+              private toastrService: ToastrService,
+              private orderService: OrderService,
+              private router: Router) {
+                const cart = cartService.getCart();
+                this.order.items = cart.items;
+                this.order.totalPrice = cart.totalPrice;
+              }
+
   ngOnInit(): void {
-    let { name, address } = this.userService.currentUser;
+    let {name, address} = this.userService.currentUser;
     this.checkoutForm = this.formBuilder.group({
-      name: [name, Validators.required],
-      address: [address, Validators.required],
+      name:[name, Validators.required],
+      address:[address, Validators.required]
     });
   }
 
@@ -37,13 +40,20 @@ export class CheckoutPageComponent {
 
   createOrder(){
     if(this.checkoutForm.invalid){
-      this.toastrService.warning('Kérem töltse ki a mezőket!', 'Érvénytelen adatok');
+      this.toastrService.warning('Kérem töltse ki az összes mezőt!', 'Érvénytelen adatok');
       return;
     }
 
     this.order.name = this.fc.name.value;
     this.order.address = this.fc.address.value;
 
-    console.log(this.order);
+    this.orderService.create(this.order).subscribe({
+      next:() => {
+        this.router.navigateByUrl('/payment');
+      },
+      error:(errorResponse) => {
+        this.toastrService.error(errorResponse.error, 'Kosár');
+      }
+    })
   }
 }
